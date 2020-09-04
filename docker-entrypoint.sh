0f3d5de3aca28af7d9d8937af03fb8620f16b441
Ryan Couse <rynocouse@gmail.com>
2020-09-04 00:28:35 -0700

    Simplify

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -23,51 +23,17 @@ if [ -z "$INPUT_SSH_PRIVATE_KEY" ]; then
     exit 1
 fi
 
-if [ -z "$INPUT_ARGS" ]; then
-  echo "Input input_args is required!"
-  exit 1
-fi
-
-if [ -z "$INPUT_DEPLOY_PATH" ]; then
-  INPUT_DEPLOY_PATH=~/docker-deployment
+if [ -z "$INPUT_PROJECT_NAME" ]; then
+    echo "Input project_name is required!"
+    exit 1
 fi
 
 if [ -z "$INPUT_STACK_FILE_NAME" ]; then
   INPUT_STACK_FILE_NAME=docker-compose.yaml
 fi
 
-if [ -z "$INPUT_DEPLOYMENT_COMMAND_OPTIONS" ]; then
-  INPUT_DEPLOYMENT_COMMAND_OPTIONS=""
-fi
-
-
-if [ -z "$INPUT_KEEP_FILES" ]; then
-  INPUT_KEEP_FILES=4
-else
-  INPUT_KEEP_FILES=$((INPUT_KEEP_FILES+1))
-fi
-
 STACK_FILE=${INPUT_STACK_FILE_NAME}
 DEPLOYMENT_COMMAND_OPTIONS=""
-
-
-if [ "$INPUT_COPY_STACK_FILE" == "true" ]; then
-  STACK_FILE="$INPUT_DEPLOY_PATH/$STACK_FILE"
-else
-  DEPLOYMENT_COMMAND_OPTIONS="$INPUT_DEPLOYMENT_COMMAND_OPTIONS --log-level debug --host ssh://$INPUT_REMOTE_DOCKER_HOST"
-fi
-
-case $INPUT_DEPLOYMENT_MODE in
-
-  docker-swarm)
-    DEPLOYMENT_COMMAND="docker $DEPLOYMENT_COMMAND_OPTIONS stack deploy --compose-file $STACK_FILE"
-  ;;
-
-  *)
-    DEPLOYMENT_COMMAND="docker-compose $DEPLOYMENT_COMMAND_OPTIONS -f $STACK_FILE"
-  ;;
-esac
-
 
 SSH_HOST=${INPUT_REMOTE_DOCKER_HOST#*@}
 
@@ -86,38 +52,10 @@ printf '%s %s\n' "$SSH_HOST" "$INPUT_SSH_PUBLIC_KEY" > /etc/ssh/ssh_known_hosts
 if ! [ -z "$INPUT_DOCKER_PRUNE" ] && [ $INPUT_DOCKER_PRUNE = 'true' ] ; then
   yes | docker --log-level debug --host "ssh://$INPUT_REMOTE_DOCKER_HOST" system prune -a 2>&1
 fi
+  
+echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: docker-compose --log-level debug --host ssh://$INPUT_REMOTE_DOCKER_HOST -f $STACK_FILE pull --ignore-pull-failures"
+DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose --log-level debug --host ssh://$INPUT_REMOTE_DOCKER_HOST -f $STACK_FILE pull --ignore-pull-failures 2>&1
 
-if ! [ -z "$INPUT_COPY_STACK_FILE" ] && [ $INPUT_COPY_STACK_FILE = 'true' ] ; then
-  execute_ssh "mkdir -p $INPUT_DEPLOY_PATH/stacks || true"
-  FILE_NAME="docker-stack-$(date +%Y%m%d%s).yaml"
+echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: docker-compose -p ${INPUT_PROJECT_NAME} --log-level debug --host ssh://$INPUT_REMOTE_DOCKER_HOST -f $STACK_FILE up -d"
+DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose -p ${INPUT_PROJECT_NAME} --log-level debug --host ssh://$INPUT_REMOTE_DOCKER_HOST -f $STACK_FILE up -d 2>&1
 
-  scp -i "$HOME/.ssh/id_rsa" \
-      -o UserKnownHostsFile=/dev/null \
-      -o StrictHostKeyChecking=no \
-      $INPUT_STACK_FILE_NAME "$INPUT_REMOTE_DOCKER_HOST:$INPUT_DEPLOY_PATH/stacks/$FILE_NAME"
-
-  execute_ssh "ln -nfs $INPUT_DEPLOY_PATH/stacks/$FILE_NAME $INPUT_DEPLOY_PATH/$INPUT_STACK_FILE_NAME"
-  execute_ssh "ls -t $INPUT_DEPLOY_PATH/stacks/docker-stack-* 2>/dev/null |  tail -n +$INPUT_KEEP_FILES | xargs rm --  2>/dev/null || true"
-
-  if ! [ -z "$INPUT_PULL_IMAGES_FIRST" ] && [ $INPUT_PULL_IMAGES_FIRST = 'true' ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
-    execute_ssh "${DEPLOYMENT_COMMAND} pull"
-  fi
-
-  if ! [ -z "$INPUT_PRE_DEPLOYMENT_COMMAND_ARGS" ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
-    execute_ssh "${DEPLOYMENT_COMMAND}  $INPUT_PRE_DEPLOYMENT_COMMAND_ARGS" 2>&1
-  fi
-
-  execute_ssh ${DEPLOYMENT_COMMAND} "$INPUT_ARGS" 2>&1
-else
-
-  if ! [ -z "$INPUT_PRE_DEPLOYMENT_COMMAND_ARGS" ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
-    execute_ssh "${DEPLOYMENT_COMMAND}  $INPUT_PRE_DEPLOYMENT_COMMAND_ARGS" 2>&1
-  fi
-
-  if ! [ -z "$INPUT_PULL_IMAGES_FIRST" ] && [ $INPUT_PULL_IMAGES_FIRST = 'true' ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
-    execute_ssh "${DEPLOYMENT_COMMAND} pull --ignore-pull-failures"
-  fi
-
-  echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS}"
-  DOCKER_HOST="tcp://127.0.0.1:2375" ${DEPLOYMENT_COMMAND} ${INPUT_ARGS} 2>&1
-fi
